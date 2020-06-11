f13661cabfd59f044c00cb8278b2aef8c66d0091
vasilieva-sasha <vasilieva.aleksandra@bk.ru>
2020-06-12 00:12:03 +0300

    устанавливает пакет propTypes и добавляет валидацию props

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,14 +1,22 @@
 import React from "react";
+import PropTypes from "prop-types";
 import {Main} from "../main/main.jsx";
 
 const App = (props) => {
-  // eslint-disable-next-line react/prop-types
-  const {title, genre, date} = props;
+  const {title, genre, date, titles} = props;
   return (
     <Main title={title}
       genre={genre}
-      date={date}/>
+      date={date}
+      titles={titles}/>
   );
+};
+
+App.propTypes = {
+  title: PropTypes.string.isRequired,
+  genre: PropTypes.string.isRequired,
+  date: PropTypes.number.isRequired,
+  titles: PropTypes.arrayOf(PropTypes.string.isRequired).isRequired,
 };
 
 export {App};
--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import {App} from "./components/app/app.jsx";
-import {Movie} from "./common/mock";
+import {Movie, TITLES} from "./common/mock";
 
 ReactDOM.render(
-    <App title={Movie.TITLE} genre={Movie.GENRE} date={Movie.DATE}/>,
+    <App title={Movie.TITLE} genre={Movie.GENRE} date={Movie.DATE} titles={TITLES}/>,
     document.querySelector(`#root`)
 );
 
